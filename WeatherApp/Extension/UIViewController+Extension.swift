a75fbccbf629e7fd993323e5099a2e70bb0cdbeb
chasomin <somin49@naver.com>
2024-02-09 16:31:51 +0900

    UIViewController Extension으로 showToast 함수 구현

--- WeatherApp/Extension/UIViewController+Extension.swift
+++ WeatherApp/Extension/UIViewController+Extension.swift
@@ -5,4 +5,16 @@
 //  Created by 차소민 on 2/9/24.
 //
 
-import Foundation
+import UIKit
+import Toast
+
+extension UIViewController {
+    func showToast(message: String) {
+        var style = ToastStyle()
+        style.backgroundColor = .white
+        style.messageColor = .black
+        
+        self.view.makeToast(message, duration: 2, position: .top, style: style)
+    }
+}
+
