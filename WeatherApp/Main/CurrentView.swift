deaed57ce6a7a8b868d2dc58e1864813a35e3789
chasomin <somin49@naver.com>
2024-02-10 15:31:28 +0900

    현재 날씨 나오는 view 구성

--- WeatherApp/Main/CurrentView.swift
+++ WeatherApp/Main/CurrentView.swift
@@ -5,4 +5,63 @@
 //  Created by 차소민 on 2/9/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class CurrentView: BaseView {
+    let cityLabel = UILabel()
+    let currentTempLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let maxMinTemp = UILabel()
+    
+    override func configureHierarchy() {
+        addSubview(cityLabel)
+        addSubview(currentTempLabel)
+        addSubview(descriptionLabel)
+        addSubview(maxMinTemp)
+    }
+    
+    override func configureLayout() {
+        cityLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview()
+        }
+        currentTempLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(cityLabel.snp.bottom).offset(5)
+        }
+        descriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(currentTempLabel.snp.bottom).offset(5)
+        }
+        maxMinTemp.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(5)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        cityLabel.font = .boldSystemFont(ofSize: 20)
+        cityLabel.textAlignment = .center
+        cityLabel.textColor = .white
+        
+        currentTempLabel.font = .boldSystemFont(ofSize: 30)
+        currentTempLabel.textAlignment = .center
+        currentTempLabel.textColor = .white
+        
+        descriptionLabel.font = .boldSystemFont(ofSize: 17)
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.textColor = .white
+        
+        maxMinTemp.font = .boldSystemFont(ofSize: 17)
+        maxMinTemp.textAlignment = .center
+        maxMinTemp.textColor = .white
+
+        
+        cityLabel.text = "도봉구"
+        currentTempLabel.text = "10"
+        descriptionLabel.text = "맑음"
+        maxMinTemp.text = "최고최저"
+    }
+}
