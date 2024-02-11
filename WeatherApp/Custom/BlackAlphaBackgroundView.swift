19c899272f751d2c1460e8aa7a1522db06cd01a9
chasomin <somin49@naver.com>
2024-02-12 02:31:46 +0900

    SearchViewController 생성

---
+++ WeatherApp/Custom/BlackAlphaBackgroundView.swift
@@ -0,0 +1,8 @@
+//
+//  BlackAlphaBackgroundView.swift
+//  WeatherApp
+//
+//  Created by 차소민 on 2/12/24.
+//
+
+import Foundation
---
+++ WeatherApp/Search/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  WeatherApp
+//
+//  Created by 차소민 on 2/12/24.
+//
+
+import UIKit
+//TODO: 역 값 전달 꼭 연습하기
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
